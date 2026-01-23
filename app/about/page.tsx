0d187577fdac7692a843c082854c6095f21f5037
Obafavour1 <olobafavour@gmail.com>
2026-01-23 11:06:34 +0100

    about error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,13 @@
 
 
+import AboutHero from "@/components/sections/about/AboutHero";
 import { AboutUs } from "@/components/sections/about/AboutUs";
 
 import CoreValues from "@/components/sections/about/CoreValues";
 import MeetTheTeam from "@/components/sections/about/MeetTheTeam";
 import MissionVision from "@/components/sections/about/MissionVision";
 import WaitList from "@/components/sections/WaitList";
-import AboutHero from "@/components/sections/about/AboutHero";
+// import AboutHero from "@/components/sections/about/AboutHero";
 
 
 const page = () => {
